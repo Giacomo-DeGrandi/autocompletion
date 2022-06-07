aab55f56dc37aaf74f5bf703d1d7dff76f01231d
giacomo-degrandi <carlo.de-grandi-giacomo@laplateforme.io>
2022-06-07 08:54:45 +0200

    redev solutions recherche pour v 1.1

--- php/Controller/Recherche_controller.php
+++ php/Controller/Recherche_controller.php
@@ -10,6 +10,44 @@ $complete = new Autocomplete();
 if(isset($_GET['search'])){
 
     $res = $complete->getAllByMulLetter($_GET['search']);
+    $all = $complete->getInsAll();
+
+    $un =[];
+
+    for($i=0;$i<=isset($all[$i]);$i++){
+        if(isset($res[$i])){
+            if($all[$i] === $res[$i]){
+                $un[]=$all[$i];
+            }
+        }
+    }
+    var_dump($un);
+    var_dump($res);
+    var_dump($all);
+
+    /*
+     *
+    for($i=0;$i<=isset($all[$i]);$i++){
+        $key = array_search($res[$i], $all[$i],true);
+        if ($key !== false) {
+            unset($all[$key]);
+        }
+    }
+
+    $all = array("a" => "red", "b" => "blue", "c" => "green");
+    $res[$i] = "blue";
+
+    $key = array_search($res[$i], $all, true);
+    if ($key !== false) {
+        unset($all[$key]);
+    }
+
+    echo json_encode($arr);
+
+
+    Output: {"a":"red","c":"green"}
+*/
 
 
 }
+
--- php/Model/Autocompletion.php
+++ php/Model/Autocompletion.php
@@ -13,11 +13,17 @@ class Autocomplete extends Model{
     }
 
     function getAllByMulLetter($input){
-        $sql = "SELECT name,id,description,data1,provenance FROM elements WHERE INSTR (name, :input )";
+        $sql = "SELECT name,id,description,data1,provenance FROM elements WHERE INSTR (name, :input ) ORDER BY name DESC";
         $p = [':input' => $input ];
         $r = $this->selectQuery($sql,$p);
         return $r->fetchAll();
     }
+    function getInsAll(){
+        $sql = "SELECT * FROM elements";
+        $r = $this->selectQuery($sql);
+        return $r->fetchAll();
+    }
+
     function getElById($id){
         $p=[':id' => $id];
         $sql = "SELECT * FROM elements WHERE id = :id";
